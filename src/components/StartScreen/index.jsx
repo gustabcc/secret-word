751677cfac21fd16673bcabeb7a5e36d36e5bff7
Gustavo Henrique <gustavo_hmoura@outlook.com>
2023-12-24 13:59:36 -0300

    first component created

---
+++ src/components/StartScreen/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// styles
+import "./StartScreen.css";
+
+export default function StartScreen() {
+  return (
+    <div className="start">
+      <h1>Secret Word</h1>
+      <p>Clique no botão abaixo para jogar</p>
+      <button>Jogar</button>
+    </div>
+  );
+}
