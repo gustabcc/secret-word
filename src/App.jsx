0812c6f05d43c2dbd24c3328ff279e9e2f6d5cbd
Gustavo Henrique <gustavo_hmoura@outlook.com>
2023-12-24 13:48:51 -0300

    initial style created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+// styles
+import "./App.css";
 export default function App() {
-  return;
+  return (
+    <div className="App">
+      <h1>Secret Word</h1>
+    </div>
+  );
 }
