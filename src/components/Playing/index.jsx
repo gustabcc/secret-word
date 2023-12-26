f7b73c15fae6900cc75f2dcd1bf8460b4088b82c
Gustavo Henrique <gustavo_hmoura@outlook.com>
2023-12-26 01:46:36 -0300

    game screen created

--- src/components/Playing/index.jsx
+++ src/components/Playing/index.jsx
@@ -4,10 +4,28 @@ import React from "react";
 import "./Playing.css";
 export default function Playing({ verifyLetter }) {
   return (
-    <div>
-      <h1>Playing</h1>
-      <p>Estou jogando</p>
-      <button onClick={verifyLetter}>Verificar letra</button>
+    <div className="game">
+      <p className="points">Pontuação: </p>
+      <h1>Adivinhe a palavra</h1>
+      <h3 className="tip">
+        Dica sobre a palavraa: <span>Dica...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blackSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente adivinhar uma letra da palavra: </p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required />
+          <button>Jogar</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:</p>
+        <span>a,</span>
+        <span>b,</span>
+      </div>
     </div>
   );
 }
